c732eb3fe15bc04fc480904d9707a40b138e58eb
farahmunh <farahiyyahmunawwar@gmail.com>
2025-01-02 10:42:45 +0800

    29th commit

--- lib/models/message.dart
+++
@@ -1,7 +0,0 @@
-class Message {
-  final String sender;
-  final String content;
-  final DateTime timestamp;
-
-  Message({required this.sender, required this.content, required this.timestamp});
-}
